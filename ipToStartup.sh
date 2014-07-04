fa5fdd721e6bdf854e8512ab7d8cc9c1e3bc9546
Jindrich Skacel <jindrich.skacel@tieto.com>
2014-07-04 17:51:28 +0200

    Initial commit

---
+++ ipToStartup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp /etc/issue /etc/issue.tmp
+echo "cp /etc/issue.tmp /etc/issue" >> /etc/rc.local
+echo "ifconfig eth0 | grep inet add | awk '{ print  }' | cut -d : -f2 >> /etc/issue" >> /etc/rc.local
